fe71929b98d08a5e67d4d10206c79c160cb11a40
juljun14 <juljun14@gmail.com>
2014-08-08 00:12:45 -0400

    create table for votes

--- db/migrate/20140808035820_create_votes.rb
+++ db/migrate/20140808035820_create_votes.rb
@@ -1,6 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+      t.integer :up
+      t.integer :down
+
+      t.belongs_to :comment
+
+      t.timestamps
     end
   end
 end
