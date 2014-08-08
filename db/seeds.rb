1fea72dd73a059d3d43606986ef56a522b6c25e1
juljun14 <juljun14@gmail.com>
2014-08-08 19:07:37 -0400

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-grequire 'faker'
+require 'faker'
 
-languages = %W(english spanish french italian korean japanese chinese arabic alien)
+languages = %W(spanish, french, italian, german, dutch, portuguese)
 level     = %W(noob decent master)
 
 20.times do |i|
@@ -9,13 +9,11 @@ level     = %W(noob decent master)
                 password_digest: "password",
                 username: Faker::Internet.user_name)
 
-  Language.create(lingua: languages[i])
+  Language.create(lingua: languages.sample)
 
   2.times do 
-    user.fluencies.create(english: "english mofo. do you speak it?",
-                   other: "nope, I don't speak english.",
-                   language_id: rand(1..10),
-                   proficiency: level.sample)
+    user.fluencies.create(language_id: rand(1..10),
+                          proficiency: level.sample)
   end
 
   Vote.create(up: rand(1..20),
