9792c36f07d1c985e07fa6fcede44fae72c418d0
juljun14 <juljun14@gmail.com>
2014-08-08 00:01:37 -0400

    create all migration files in alphabetical order

---
+++ db/migrate/20140808035755_create_fluencies.rb
@@ -0,0 +1,6 @@
+class CreateFluencies < ActiveRecord::Migration
+  def change
+    create_table :fluencies do |t|
+    end
+  end
+end
---
+++ db/migrate/20140808035809_create_queries.rb
@@ -0,0 +1,6 @@
+class CreateQueries < ActiveRecord::Migration
+  def change
+    create_table :queries do |t|
+    end
+  end
+end
