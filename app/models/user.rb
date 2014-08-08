2d863785059c4c4e25236721afd767949445bd6e
Farheen Malik <fma2@github.com>
2014-08-08 16:36:06 -0400

    Add user association with fluencies and languages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :comments, :as => :commentable, :dependent => :destroy  
+  has_many :comments, :as => :commentable, :dependent => :destroy
+
+  has_many :fluencies
+  has_many :languages, through: :fluencies  
 end
