787abbfbe74111e1f415e3bd825a5345f3ba1660
juljun14 <juljun14@gmail.com>
2014-08-08 17:35:49 -0400

    seeeeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+languages = %W(english spanish french italian korean japanese chinese arabic alien)
+
+20.times do |i|
+  User.create(first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+                password_digest: "password",
+                username: Faker::Internet.user_name)
+
+  Language.create(lingua: languages[i])
+
+  Fluency.create(english: "english mofo. do you speak it?",
+                   other: "nope, I don't speak english.",
+                   language_id: rand(1..10),
+                   user_id: rand(1.10))
+
+  Vote.create(up: rand(1..20),
+              down: rand(1..20),
+              comment_id: rand(1..10))
+
+  Comment.create(content: "And the cow jumped over the moon.",
+                 commentable: User.first.comments.first)
+end
