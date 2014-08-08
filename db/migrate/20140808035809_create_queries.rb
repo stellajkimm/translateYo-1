2258d99f87137caa4798768debb5b15695665899
juljun14 <juljun14@gmail.com>
2014-08-08 00:06:20 -0400

    create table for queries

--- db/migrate/20140808035809_create_queries.rb
+++ db/migrate/20140808035809_create_queries.rb
@@ -1,6 +1,13 @@
 class CreateQueries < ActiveRecord::Migration
   def change
     create_table :queries do |t|
+      t.string :english
+      t.string :other
+
+      t.belongs_to :language
+      t.belongs_to :user
+
+      t.timestamps
     end
   end
 end
