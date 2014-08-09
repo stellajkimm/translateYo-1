a803f567d0805b15ea270a5a7cf6bb9411524491
Farheen Malik <fma2@github.com>
2014-08-09 17:15:55 -0400

    Add skeleton of factories and begin to add TDD for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe CommentsController, :type => :controller do
-	let!(:query) {FactoryGirl.create :query}
+	# let!(:query) {FactoryGirl.create :query}
 	let!(:comment) {FactoryGirl.create :comment}
 
 	context "#new" do
-		
+		get :new
+		expect(assigns(:comment)).to eq(Comment.last)
 	end
 
 	context "#create" do
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,24 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n| "email#{n}@gmail.com" }
+  	sequence(:username) { |n| "user#{n}"}
+    # sequence(:email) {|n| "email#{n}@gmail.com" }
     password "password"
   end
+  
+  factory :fluency do
 
-  # factory :post do
-  #   sequence(:title) { |n| "New post#{n}" }
-  #   content "A great post."
-  # end
+  end
+
+  factory :language do
+
+  end
+
+  factory :query do
+
+  end
+
+  factory :comment do
+  	sequence(:content) {|n| "Here is comment no. #{n}."}
+  end
+  
 end
