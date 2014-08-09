c2088aa865e06684398fb2f5877d72fcd3715eb5
Farheen Malik <fma2@github.com>
2014-08-09 18:50:17 -0400

    Add beginning of test for view of new comment page

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,23 +1,30 @@
 require 'rails_helper'
 
-describe 'Comments Panel' do
-	it "user can see comments on queried translation" do
-		pending
-	end
-
-	context "when logged in" do
-		it "user can make comments on a query by clicking comment link" do
-			pending
-  	end
-
-  	it "user can make comments on comments of a query by clicking nested comment link" do
-  		pending
-  	end
-	end
-
-	context "when not logged in" do
-		it "user can't make comments on a query after clicking comment link" do
-			pending
+describe 'Comment features' do
+	context "on new comments page" do
+		let(:query) { FactoryGirl.create(:query) }
+		it "can see a form to write a new comment" do
+			visit new_query_comment_path(query)
 		end
 	end
+
+	# it "user can see comments on queried translation" do
+	# 	pending
+	# end
+
+	# context "when logged in" do
+	# 	it "user can make comments on a query by clicking comment link" do
+	# 		pending
+ #  	end
+
+ #  	it "user can make comments on comments of a query by clicking nested comment link" do
+ #  		pending
+ #  	end
+	# end
+
+	# context "when not logged in" do
+	# 	it "user can't make comments on a query after clicking comment link" do
+	# 		pending
+	# 	end
+	# end
 end
