a5a98c3dc4d55531d517fdf35facf36901957ada
Farheen Malik <fma2@github.com>
2014-08-08 21:07:09 -0400

    Modify comments controllers spec and added factories file - not with correct info

---
+++ spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email) {|n| "email#{n}@gmail.com" }
+    password "password"
+  end
+
+  factory :post do
+    sequence(:title) { |n| "New post#{n}" }
+    content "A great post."
+  end
+end
