1f55b708e2ba4dc4069cb5693b07b75141b69e36
Farheen Malik <fma2@github.com>
2014-08-09 15:55:19 -0400

    move validation in comments model to above query method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,13 +5,15 @@ class Comment < ActiveRecord::Base
 
 	# has_many :votes
 
-	 belongs_to :commentable, :polymorphic => true
- 	 has_many :comments, :as => :commentable
+	belongs_to :commentable, :polymorphic => true
+ 	has_many :comments, :as => :commentable
+
+ 	 validates :content, presence: true
   
   def query
     return @query if defined?(@query)
     @query = commentable.is_a?(Query) ? commentable : commentable.query
   end
 
-	validates :content, presence: true
+
 end
