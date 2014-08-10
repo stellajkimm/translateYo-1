c343a90ee22f57f5118d399ed84c9028b3912c67
Farheen Malik <fma2@github.com>
2014-08-10 02:22:45 -0400

    Change redirect to languages_path

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -4,7 +4,7 @@ module Authentication
   end
 
   def authorize_user!
-    redirect_to language_queries_path unless current_user.present?
+    redirect_to languages_path unless current_user.present?
   end
 
   def login user
