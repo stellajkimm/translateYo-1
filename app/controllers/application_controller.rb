a5194ce54fdac04142d3a4a4ab21b3cfd2b0924b
juljun14 <juljun14@gmail.com>
2014-08-10 15:56:18 -0400

    clean up views and fix modals to show signin and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   include Authentication
 
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    "http://www.google.com" # <- Path you want to redirect the user to.
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, :except => [:index]  
-
-  def index
-    redirect_to :root
-  end
-
-  def test
-    flash[:notice] = true
-    redirect_to '/'
-  end
-
-  def login
-    @user = User.find_by(email: params[:user][:email])
-    if user_signed_in?
-      session[:user] = @user.id
-      redirect_to user_ratings_path(@user), flash: {notice: "Successful log in!"}
-    else
-      redirect_to users_path, flash: {notice: 'Invalid credentials!' }
-    end
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params(params))
-    if @user.save
-      session[:user] = @user.id
-      redirect_to user_ratings_path(@user), flash: {notice: 'Successful log in!'}
-    else
-      redirect_to new_user_path, flash: {notice: 'Failed'}
-    end
-  end
-
-  def sign_out
-    session.clear
-    redirect_to "/"
-  end
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+   before_filter :authenticate_user!, except: [:new, :create]
 
   def show
-      @user = User.find(params[:id])
-  end
-
- private
-
-  def user_params(params)
-    params.require(:user).permit(:name, :email, :password)
+  @user = User.find(params[:id])
   end
 
 end
