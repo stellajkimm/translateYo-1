e2bec2a33af784ff09f8d54a495b07eb73460e61
Farheen Malik <fma2@github.com>
2014-08-10 00:31:20 -0400

    Begin test for whether or not query is defined

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe Comment do
+	let(:query) {FactoryGirl.create(:query)}
 	context "validations" do
-		pending
+		it { should validate_presence_of :content }
 	end	
 
-	context "#" do
-		pending
+	context "query" do
+		it "returns query if defined" do
+			# expect(assigns(:query)).to eq(@query)
+		end
 	end
 
 end
