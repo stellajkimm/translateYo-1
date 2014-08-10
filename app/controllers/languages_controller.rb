91f7776f02f91bfa114a1f267c32025e437c1daa
Farheen Malik <fma2@github.com>
2014-08-09 23:58:25 -0400

    Fix merge conflict with merge upstream master

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-class LanguagesController < ApplicationControllerg
-=======
 class LanguagesController < ApplicationController
 	def index
 	end
@@ -9,5 +6,4 @@ class LanguagesController < ApplicationController
 		@language = Language.find(params[:id])
 		@queries = @language.queries
 	end
->>>>>>> 2126637d870f96dcaa5853fb8b806104e9525994
 end
