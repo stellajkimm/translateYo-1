40b3d1181c443533b04e1d9660249d946966fbe3
Farheen Malik <fma2@github.com>
2014-08-10 03:09:17 -0400

    Add tests for up-votes and down-votes and query method

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 describe Comment do
-	let(:query) {FactoryGirl.create(:query)}
+	let!(:language) { FactoryGirl.create(:language) }
+	let!(:query) {FactoryGirl.create(:query, :language => language)}
+	let!(:comment) { FactoryGirl.create(:comment, :commentable => query)}
+
 	context "validations" do
 		it { should validate_presence_of :content }
 	end	
 
-	context "query" do
-		it "returns query if defined" do
-			pending
-			# expect(assigns(:query)).to eq(@query)
+	context "comment votes" do
+		it "should be 0 up_votes by default" do
+			expect(comment.up_vote).to eq(0)
+		end
+
+		it "should be 0 down_votes by default" do
+			expect(comment.down_vote).to eq(0)
 		end
 	end
 
+	it "when commentable is defined, returns @query" do
+		# expect(comment.query).to_not eq(nil)
+		expect(comment.commentable).to eq(query)
+	end
 end
