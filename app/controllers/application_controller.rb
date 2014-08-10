06bdc111198114b9b2d21921ae3d4bed4b0d1eea
juljun14 <juljun14@gmail.com>
2014-08-10 15:57:22 -0400

    clean up application controller that was used for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    "http://www.google.com" # <- Path you want to redirect the user to.
-  end
 end
