ee84687228a143b15346e683aa2d9bbe9f3754af
Farheen Malik <fma2@github.com>
2014-08-10 01:32:07 -0400

    Fix first test per changes in comments_controller to pass

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,8 +4,9 @@ describe CommentsController, :type => :controller do
 
 	let!(:query) { FactoryGirl.create :query }
 
-	context "GET #new" do
 
+	context "GET #new" do
+		
 		it "assigns a new comment to @comment" do
 			stub_authorize_user!
 			get :new
@@ -18,19 +19,22 @@ describe CommentsController, :type => :controller do
 		subject { FactoryGirl.create(:comment) }
 		
 		it "should create a comment when params are valid" do
-		# 	post :create
-		# 	expect { subject }.to change{Comment.count}.by(1)
+			stub_authorize_user!
+			post :create
+			expect { subject }.to change{Comment.count}.by(1)
 		end
 
 		it "should redirect to query page on successful save" do
-			# post :create
-			# expect(subject).to redirect_to(queries_path)
+			stub_authorize_user!
+			post :create
+			expect(subject).to redirect_to(queries_path)
 		end
 
 		it "should re-render new template on failed save" do #BUGBUG
-			# post :create
-			# comment = FactoryGirl.build(:comment, content: nil)
-	  #   expect(response).to render_template("comments/new")
+			stub_authorize_user!
+			post :create
+			comment = FactoryGirl.build(:comment, content: nil)
+	    expect(response).to render_template("comments/new")
   	end
 	end
 
