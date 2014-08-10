9de454dede159b428c549be8cab598c15a2d52f8
Farheen Malik <fma2@github.com>
2014-08-10 15:40:41 -0400

    Add text for debugging - removed it for this commit

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -3,11 +3,7 @@ class LanguagesController < ApplicationController
 	end
 
 	def show
-		p "?" * 50
-		p params
-
 		@language = Language.find(params[:id])
 		@queries = @language.queries
-		p @queries
 	end
 end
