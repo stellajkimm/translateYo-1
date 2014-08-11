9f61e228e8b5ba11ccfe2a8bb5185302c7864f75
Farheen Malik <fma2@github.com>
2014-08-10 20:06:42 -0400

    Change has_many comments association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :comments, :as => :commentable, :dependent => :destroy
+  has_many :comments
   has_many :fluencies
   has_many :languages, through: :fluencies  
 end
