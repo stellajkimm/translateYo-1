b334d7d1d9a036508ce794779d8d4772c0308fb7
Stella Kim <stellajkimm@gmail.com>
2014-08-11 09:20:50 -0400

    add upvote downvote buttons

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,7 +7,7 @@ describe CommentsController, :type => :controller do
   # let!(:comment) {FactoryGirl.create :comment}
 
 	context "GET #new" do
-		
+
 		it "assigns a new comment to @comment" do
 			stub_authorize_user!
 			get :new, query_id: query.id
@@ -18,19 +18,19 @@ describe CommentsController, :type => :controller do
 
 	context "POST #create" do
 		render_views
-		
+
 		it "should create a comment when params are valid" do
 			stub_authorize_user!
-			expect { 
+			expect {
 
-				post :create, query_id: query.id, comment: { content: 'my comment' } 
+				post :create, query_id: query.id, comment: { content: 'my comment' }
 				}.to change{Comment.count}.by(1)
 		end
 
 		it "should redirect to query page on successful save" do
 			stub_authorize_user!
 			expect(
-				post :create, query_id: query.id, comment: { content: 'my comment' } 
+				post :create, query_id: query.id, comment: { content: 'my comment' }
 				).to redirect_to(query_path(query.id))
 			# expect(response).to redirect_to(query_comments_path(query)) #Should be this but there is a problem with routes
 
@@ -38,9 +38,12 @@ describe CommentsController, :type => :controller do
 
 		# it "should notify of redirect to new template on failed save" do #BUGBUG
 		# 	stub_authorize_user!
-		# 	post :create, query_id: query.id, comment: { content: nil } 
+		# 	post :create, query_id: query.id, comment: { content: nil }
 	 #    expect(response.body).to eq("<html><body>You are being <a href=\"http://test.host/languages\">redirected</a>.</body></html>")
   # 	end
+	end
+
+	context "#up_vote" do
 	end
 
 
